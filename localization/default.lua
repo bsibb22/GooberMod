407272d5d6398529ded9b748c9ad82becbf442d6
bsibb22 <b.sibbitt@ufl.edu>
2025-05-14 14:04:09 -0400

    DOUBLE EFFECT JOKER FINALLY IMPLEMENTED

---
+++ localization/default.lua
@@ -0,0 +1,21 @@
+return {
+	descriptions = {
+		Joker = {
+			j_goob_joker_cursed_0 = {
+				name = "Cursed Joker",
+				text = {
+					'-{C:attention}#1#{} hand size'
+				}
+			},
+			j_goob_joker_cursed_1 = {
+				name = "Cursed Joker (active)",
+				text = {
+					'This Joker gains {X:mult,C:white}x#2#{}',
+					'Mult whenever a',
+					'Consumable is used',
+					'{C:inactive}(Currently {}{X:mult,C:white}x#3#{}{C:inactive} Mult)'
+				}
+			}
+		}
+	}
+}
